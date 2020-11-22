4e5e538948b73084788e68a8e236c4290362204d
kk-boop <56920559+kk-boop@users.noreply.github.com>
2020-11-22 22:28:27 +0200

    add proper header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,46 @@
 <template>
+  <header>
+    <nav>
+      <div class="logo-container">
+        <img src="@/assets/logo.png" alt="postIt">
+      </div>
+      <div class="search-container">
+        <input type="text" name="search"><button type="button">Search</button>
+      </div>
+      <div class="avatar-container">
+        <img class="avatar" @click="showDropdown = !showDropdown"  v-bind:src="$store.state.userInfo.avatar">
+        <div v-if="showDropdown" class="drop-down-container">
+          <span id="user-name">{{ $store.state.userInfo.firstname + " " + $store.state.userInfo.lastname }}</span>
+          <span id="user-email">{{ $store.state.userInfo.email }}</span>
+          <span class="separator"></span>
+          <span>
+            <router-link to="/browse">Browse</router-link> |
+          </span>
+          <span class="separator"></span>
+          <span>
+            <router-link to="/login">Log out</router-link>
+          </span>
+        </div>
+      </div>
+     </nav>
+  </header>
   <div id="nav" v-if="$route.name !== 'About'">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
   </div>
   <router-view/>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  mounted() {
+    this.$store.dispatch('getUserInfo')
+  }
+})
+export default class App extends Vue {
+  showDropdown = false
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -35,6 +71,79 @@ body {
 
 a {
   color: #40c4ff;
+}
+
+.drop-down-container {
+    position: absolute;
+    min-width: 150px;
+    height: auto;
+    background-color: #ffffff;
+    padding: 10px;
+    right: 0;
+    top: 50px;
+    text-align: left;
+}
+.drop-down-container span{
+    display: block;
+}
+.drop-down-container span.separator{
+    border-bottom: 1px solid #d7d7d7;
+    margin: 10px -10px;
+}
+
+header {
+    position: fixed;
+    width: 100%;
+    top: 0;
+    z-index: 1;
+}
+
+header:hover {
+    box-shadow: 0 -20px 30px #4d4d4d;
+}
+
+nav {
+    display: flex;
+    background-color: #ffffff;
+    align-items: center;
+}
+
+nav div {
+    height: 30px;
+    flex-grow: 4;
+    padding: 10px;
+}
+
+nav div img {
+    height: 100%;
+    width: 30px;
+    margin-left: 15px;
+    border-radius: 100%;
+    object-fit: cover;
+    object-position: top center;
+}
+
+nav div.search-container > input {
+    box-sizing: border-box;
+    height: 30px;
+    width: 80%;
+    margin: 0;
+    padding: 5px;
+    border: 1px solid #e0e0e0;
+}
+
+nav div.search-container > button {
+    height: 30px;
+    width: 20%;
+    margin: 0;
+    padding: 5px;
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+}
+
+nav div.avatar-container {
+    margin-right: 15px;
+    text-align: right;
 }
 
 #nav {
