608f35900bc07b76b042c741d94acf80dbbd7895
MehisXD <mehist8@gmail.com>
2020-11-22 20:00:42 +0200

    poop

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,30 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  background-color: #0277bd;
   text-align: center;
   color: #2c3e50;
+}
+
+* {
+  font-family: 'Roboto Slab', serif;
+  outline: none;
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+
+body {
+  background-color: #0277bd;
+  color: #263238;
+}
+
+a {
+  color: #40c4ff;
 }
 
 #nav {
