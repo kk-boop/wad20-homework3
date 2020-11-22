120d2de6726ef17eb0a606da413d15e34a346f20
MehisXD <mehist8@gmail.com>
2020-11-22 21:42:19 +0200

    post most work

---
+++ src/components/Post.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="post">
+        <span class="post-author">
+          <span class="post-author-info">
+          <img v-bind:src="post.author.avatar">
+          <small> {{post.author.firstname}} {{post.author.lastname}}</small>
+          </span>
+          <small>{{ post.createTime }}</small>
+        </span>
+
+    <div class="post-image" v-if="post.media">
+      <img v-if="post.media.type === 'image' " v-bind:src="post.media.url" >
+      <video controls v-if="post.media.type === 'video'">
+        <source v-bind:src="post.media.url">
+      </video>
+    </div>
+
+    <div class="post-title">
+      <h3>{{ post.text }}</h3>
+    </div>
+
+    <div class="post-actions">
+      <button type="button" class="like-button"></button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  props: {
+    post: Object
+  }
+})
+export default class Post extends Vue {
+  post!: any
+}
+</script>
+
+<style scoped>
+
+li .post-image img, video {
+  width: 100%;
+  min-height: 150px;
+  max-height: 350px;
+  object-fit: cover;
+  object-position: top center;
+}
+
+.post {
+  width: 80%;
+  margin: 15px auto;
+  box-shadow: 0 0 15px rgba(38, 50, 56, 0.33);
+  border-radius: 5px;
+}
+
+.post .post-author {
+  padding: 10px;
+}
+
+.post .post-author::after {
+  content: "";
+  display: block;
+  clear: both;
+}
+
+.post .post-author .post-author-info {
+  float: left;
+  position: relative;
+  width: 50%;
+}
+
+.post .post-author .post-author-info img {
+  width: 30px;
+  height: 30px;
+  border-radius: 100%;
+  object-fit: cover;
+  object-position: top;
+  margin: 5px;
+}
+
+.post .post-author .post-author-info small {
+  position: absolute;
+  top: 10px;
+  left: 35px;
+}
+
+.post .post-author .post-author-info + small {
+  float: right;
+  color: grey;
+  padding: 10px;
+}
+
+.post .post-image img, video {
+  width: 100%;
+  min-height: 150px;
+  max-height: 350px;
+  object-fit: cover;
+  object-position: top center;
+}
+
+.post .post-title {
+  padding: 10px;
+}
+
+.post .post-title h3 {
+  display: inline;
+}
+
+.post .post-title ~ .post-actions {
+  padding: 10px;
+}
+.like-button {
+  background-image: url("../assets/like.png");
+  background-size: 15px;
+  background-repeat: no-repeat;
+  background-position: 5px center;
+  background-color: #8a8a8a;
+  width: 60px;
+  height: 25px;
+  padding-left: 23px;
+  line-height: 10px;
+  text-align: left;
+  border: none;
+}
+
+.like-button.liked {
+  background-color: #01579b;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,43 @@
 <template>
-  <div class="home">
-     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-   </div>
- </template>
+  <div class="hello">
+    <div v-for='post in posts' v-bind:key="post.id" class="post">
+      <Post v-bind:post="post"></Post>
+    </div>
+  </div>
+</template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import Post from '@/components/Post.vue' // @ is an alias to /src
 
 @Options({
+  computed: {
+    posts() {
+      return this.$store.state.posts
+    }
+  },
+  mounted() {
+    this.$store.dispatch('getPosts')
+  },
   components: {
-    HelloWorld
+    Post
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+}
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.hello {
+  width: 50%;
+  min-height: 100%;
+  margin: auto auto;
+  padding: 90px 15px 15px 15px;
+  background-color: #ffffff;
+}
+a {
+  color: #42b983;
+}
+</style>
